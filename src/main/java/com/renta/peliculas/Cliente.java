07796461f10c55271b0fefa7c0a7c334fef7062a
Rogelio <rsanchezc@ucenfotec.ac.cr>
2018-04-19 19:08:56 -0600

    Se implementa la el enum en la clase Cliente para hacer el calculo de los alquileres depurando el comando switch con los calculos, la razón es permitir es con el enum permitir agregar nuevas categorias con mayor facilidad

--- src/main/java/com/renta/peliculas/Cliente.java
+++ src/main/java/com/renta/peliculas/Cliente.java
@@ -2,13 +2,20 @@ package com.renta.peliculas;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import com.renta.peliculas.Categoria;
 
 public class Cliente {
 	private String nombre;
+	private ArrayList<Alquiler> alquileres;
 	
 	public Cliente(String pNombre) {
         nombre = pNombre;
     }
+	
+	public Cliente(String pNombre, ArrayList<Alquiler> pAlquileres){
+		nombre = pNombre;
+		alquileres = pAlquileres;
+	}
 	
     public String statement() {
         double montoTotal = 0;
@@ -20,27 +27,31 @@ public class Cliente {
             Alquiler alquiler = iterator.next();
 
             //determine amounts for each line
-            switch (alquiler.getDisco().getPelicula().getCodigoPrecio()) {
-                case Pelicula.NORMAL:
+            Categoria tipoPelicula = Categoria.values()[alquiler.getDisco().getPelicula().getCodigoPrecio()];           
+            switch (tipoPelicula) {
+                case NORMAL:                	
                     montoAlquiler += 2;
                     if (alquiler.getDiasAlquilado() > 2)
                         montoAlquiler += (alquiler.getDiasAlquilado() - 2) * 1.5;
                     break;
-                case Pelicula.ESTRENO:
+                case ESTRENO:
                     montoAlquiler += alquiler.getDiasAlquilado() * 3;
                     break;
-                case Pelicula.INFANTIL:
+                case INFANTIL:
                     montoAlquiler += 1.5;
                     if (alquiler.getDiasAlquilado() > 3)
                         montoAlquiler += (alquiler.getDiasAlquilado() - 3) * 1.5;
                     break;
-
+                 default:
+                	 System.out.println("Categoria no existente");
+                	 break;
             }
             montoTotal += montoAlquiler;
 
             puntosAlquilerFrecuente ++;
+            
             // agregar bono por alquiler de pelicula "estreno"
-            if ((alquiler.getDisco().getPelicula().getCodigoPrecio() == Pelicula.ESTRENO) && alquiler.getDiasAlquilado() > 1) puntosAlquilerFrecuente ++;
+            if ((alquiler.getDisco().getPelicula().getCodigoPrecio() == 1) && alquiler.getDiasAlquilado() > 1) puntosAlquilerFrecuente ++;
 
             //mostrar datos
             result += "\t" + alquiler.getDisco().getPelicula().getNombre()+ "\t" + String.valueOf(montoAlquiler) + "\n";
@@ -52,10 +63,6 @@ public class Cliente {
         return result;
 
     }
-	public String getNombre() {
-		return nombre;
-	}
-    
     
     public void addRental(Alquiler arg) {
     	alquileres.add(arg);
@@ -63,8 +70,13 @@ public class Cliente {
     public static Cliente get(String name) {
     	return (Cliente) Registrar.get("Clientes", name);
     }
+    
+	public String getNombre() {
+		return nombre;
+	}
+	
     public void persist() {
     	Registrar.add("Clientes", this);
     }
-    private ArrayList<Alquiler> alquileres= new ArrayList<>();
+    
 }
