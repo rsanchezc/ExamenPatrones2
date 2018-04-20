36e5266515500de1b8ea9e49c092ea14b3887493
Rogelio <rsanchezc@ucenfotec.ac.cr>
2018-04-19 19:28:37 -0600

    Se crean los test para probar la clase Cliente con cada tipo de categoria y los resultados base de alquileres distintos NORMAL con valor 0, ESTRENO con valor 1 e INFANTIL con valor 2

--- src/test/java/com/renta/peliculas/ClienteTest.java
+++ src/test/java/com/renta/peliculas/ClienteTest.java
@@ -2,7 +2,9 @@ package com.renta.peliculas;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import org.junit.Test;
 
@@ -26,18 +28,78 @@ public class ClienteTest {
 		Clientes.put("ID001", clienteTest);
 						
 		assertEquals("SuNombre", Clientes.get("ID001").getNombre());		
-	}
+	}	
 	
 	@Test
-	public void testStatementVacio() {
+	public void testStatementConAlquilerTipoNormal() {
+				
+		ArrayList alquileres = new ArrayList();									
 		
-		Cliente clienteTest = new Cliente("SuNombre");		
+		Pelicula pelicula = new Pelicula("NPelicula",0);
+		Disco disco = new Disco("S101",pelicula);	
+		Alquiler alquilerTest = new Alquiler(disco,2);
+		
+		alquileres.add(alquilerTest);
+						
+					
+		Cliente clienteTest = new Cliente("SuNombre",alquileres);
+		
 		String mensaje;
 		
 		mensaje ="Alquileres de " + "SuNombre" + ":\n";
-		mensaje +=  "Monto total:  " + "0.0" + "\n";
-        mensaje += "Gano " + "0" + " puntos por alquiler frecuente";
+		mensaje += "\t" +"NPelicula" + "\t" + "2.0" + "\n";
+		mensaje +=  "Monto total:  " + "2.0" + "\n";
+        mensaje += "Gano " + "1" + " puntos por alquiler frecuente";
+        
+        assertEquals(mensaje, clienteTest.statement());		
+	}
+	
+	
+	@Test
+	public void testStatementConAlquilerTipoEstreno() {
+				
+		ArrayList alquileres = new ArrayList();									
 		
+		Pelicula pelicula = new Pelicula("NPelicula",1);
+		Disco disco = new Disco("S101",pelicula);	
+		Alquiler alquilerTest = new Alquiler(disco,2);
+		
+		alquileres.add(alquilerTest);
+						
+					
+		Cliente clienteTest = new Cliente("SuNombre",alquileres);
+		
+		String mensaje;
+		
+		mensaje ="Alquileres de " + "SuNombre" + ":\n";
+		mensaje += "\t" +"NPelicula" + "\t" + "6.0" + "\n";
+		mensaje +=  "Monto total:  " + "6.0" + "\n";
+        mensaje += "Gano " + "2" + " puntos por alquiler frecuente";
+        
+        assertEquals(mensaje, clienteTest.statement());		
+	}
+	
+	@Test
+	public void testStatementConAlquilerTipoInfantil() {
+				
+		ArrayList alquileres = new ArrayList();									
+		
+		Pelicula pelicula = new Pelicula("NPelicula",2);
+		Disco disco = new Disco("S101",pelicula);	
+		Alquiler alquilerTest = new Alquiler(disco,2);
+		
+		alquileres.add(alquilerTest);
+						
+					
+		Cliente clienteTest = new Cliente("SuNombre",alquileres);
+		
+		String mensaje;
+		
+		mensaje ="Alquileres de " + "SuNombre" + ":\n";
+		mensaje += "\t" +"NPelicula" + "\t" + "1.5" + "\n";
+		mensaje +=  "Monto total:  " + "1.5" + "\n";
+        mensaje += "Gano " + "1" + " puntos por alquiler frecuente";
+        
         assertEquals(mensaje, clienteTest.statement());		
 	}
 	
